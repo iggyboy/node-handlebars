8c9ad82918a0cee85832e46e29d7ec91c005f460
William Lukas <iggyboy456@gmail.com>
2019-10-17 20:23:31 -0500

    fixed connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "yhrz9vns005e0734.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "dbpassword",
-  database: "burger_db"
+  user: "ux0gavbxvt49qt8l",
+  password: "t0bq1teonh14lh95",
+  database: "o9023mc95r3c01sg"
 });
 
 connection.connect(function(err) {
